12253f72921d3fd80d19dc795ebcfb42efc7136d
Moises Alves <moisesalvespc@gmail.com>
2023-12-09 15:47:54 -0300

    loading add

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import api from './src/Services/api';
 import Filmes from './src/Filmes';
 
 export default function App() {
   const [filmes, setFilmes] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     async function loadFilmes() {
@@ -12,21 +13,33 @@ export default function App() {
       setFilmes(response.data)
       console.log(filmes)
 
+      setLoading(false)
+
     }
     loadFilmes()
 
   }, [])
+
+  if (loading) {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <ActivityIndicator color='black' size={45} />
+      </View>)
+
+
+
+  }
   return (
     <View style={styles.container}>
       <FlatList
-      data={filmes}
-      renderItem={({item})=> <Filmes data={item}/>}/>
+        data={filmes}
+        renderItem={({ item }) => <Filmes data={item} />} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1
   }
 })
